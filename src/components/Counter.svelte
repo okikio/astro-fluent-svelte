bd5315629c9f7f11b956a6dc35f5bc214248e851
Okiki <okikio.dev@gmail.com>
2022-03-01 05:08:31 +0000

    feat: initial release

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -8,5 +8,13 @@
 <TextBlock variant="display">{count}.</TextBlock>
 <div>
     <Button variant="accent" on:click={() => count++}>Increase Counter</Button>
-    <Button on:click={() => count = 0}>Reset Counter</Button>
+    <Button on:click={() => count--}>Decrease Counter</Button>
+    <Button variant="hyperlink" on:click={() => count = 0}>Reset Counter</Button>
 </div>
+
+<style>
+	:global(body) {
+		background-color: var(--fds-solid-background-base);
+		color: var(--fds-text-primary);
+	}
+</style>
